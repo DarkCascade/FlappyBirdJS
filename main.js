ac8c55ecbe9717ecfccc9ef13b0f48c322d2d453
DarkCascade <darkcascade@gmail.com>
2014-04-08 12:42:16 -0600

    first phase done

--- main.js
+++ main.js
@@ -6,15 +6,66 @@ var main_state = {
 
     preload: function() { 
 		// Function called first to load all the assets
+		this.game.stage.backgroundColor = '#71c5cf';
+		this.game.load.image('bird', 'assets/bird.png');
+		this.game.load.image('pipe', 'assets/pipe.png');
     },
 
     create: function() { 
-    	// Fuction called after 'preload' to setup the game    
+    	// Function called after 'preload' to setup the game    
+		this.bird = this.game.add.sprite(100, 245, 'bird');
+		this.bird.body.gravity.y = 1000;
+		
+		this.pipes = game.add.group();
+		this.pipes.createMultiple(20, 'pipe');
+		
+		var spacebar = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+		spacebar.onDown.add(this.jump, this);
+		
+		this.timer = this.game.time.events.loop(1500, this.add_row_of_pipes, this);
+		
+		this.score = 0;
+		var style = { font: "30px Arial", fill: "#ffffff" };
+		this.label_score = this.game.add.text(20, 20, "0", style);
     },
     
     update: function() {
 		// Function called 60 times per second
+		if (this.bird.inWorld == false)
+			this.restart_game();
+			
+		this.game.physics.overlap(this.bird, this.pipes, this.restart_game, null, this);
     },
+	
+	jump: function() {
+		this.bird.body.velocity.y = -350;
+	},
+	
+	restart_game: function() {
+		this.game.time.events.remove(this.timer);
+		
+		// Start the 'main' state, which restarts the game
+		this.game.state.start('main');
+	},
+	
+	add_one_pipe: function(x, y) {
+		// Get the first dead pipe in the group
+		var pipe = this.pipes.getFirstDead();
+		pipe.reset(x, y);
+		pipe.body.velocity.x = -200;
+		pipe.outOfBoundsKill = true;
+	},
+	
+	add_row_of_pipes: function(x, y) {
+		var hole = Math.floor(Math.random() * 5) + 1;
+		
+		for (var i = 0; i < 8; i++)
+			if (i != hole && i != hole + 1)
+				this.add_one_pipe(400, i * 60 + 10);
+				
+		this.score += 1;
+		this.label_score.content = this.score;
+	},
 };
 
 // Add and start the 'main' state to start the game
